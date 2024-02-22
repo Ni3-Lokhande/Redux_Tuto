b1ec0b237ae4d384139d5a75cbb0f6448839ce0e
Nitin Lokhande <nitinlokhande1995@gmail.com>
2024-02-22 17:16:39 +0530

    hiiii

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import User from './User';
 function App() {
   return (
     <div className="App">
-     {/* <h1>App Component</h1> */}
      <User data={{name : 'Nitin Lokhande',age :'27'}}/>
     </div>
   );
